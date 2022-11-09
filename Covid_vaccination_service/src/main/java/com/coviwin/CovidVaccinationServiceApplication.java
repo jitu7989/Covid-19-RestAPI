cb1fa89cdf42451eb1f2811671b09a8c431d7fad
jitu7989 <choudharyjitendra7989@gmail.com>
2022-11-09 09:43:22 +0530

    init

---
+++ Covid_vaccination_service/src/main/java/com/coviwin/CovidVaccinationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coviwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccinationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccinationServiceApplication.class, args);
+	}
+
+}
