5d4ac4554f9ee900b54002c8cc25cb91fed8b5cf
jitu7989 <choudharyjitendra7989@gmail.com>
2022-11-10 10:36:47 +0530

    two interface ready

---
+++ Covid_vaccination_service/src/main/java/com/coviwin/exception/MemberException.java
@@ -0,0 +1,12 @@
+package com.coviwin.exception;
+
+public class MemberException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
--- Covid_vaccination_service/src/main/java/com/coviwin/service/MemberService.java
+++ Covid_vaccination_service/src/main/java/com/coviwin/service/MemberService.java
@@ -1,5 +1,15 @@
 package com.coviwin.service;
 
-public interface MemberService {
+import com.coviwin.exception.MemberException;
+import com.coviwin.model.Member;
 
+public interface MemberService {
+		
+	public Member getMemberbyId(Integer idCardID ) throws MemberException;
+	public Member getMemberByAdharNo(Long adharNo) throws MemberException;
+	public Member getMemberByPanNo( String panNo ) throws MemberException;
+	public Member addMember( Member member ) throws MemberException;
+	public Member updateMember( Member member ) throws MemberException;
+	public Boolean deleteMember( Member member ) throws MemberException;
+	
 }
