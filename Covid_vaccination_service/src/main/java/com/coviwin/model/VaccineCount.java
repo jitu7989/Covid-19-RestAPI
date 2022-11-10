81afa423be96e9a6c697d95cda0d7988dc05335f
1301yashraj <1301yashraj@gmail.com>
2022-11-10 12:30:10 +0530

    Day 2 Done

--- Covid_vaccination_service/src/main/java/com/coviwin/model/VaccineCount.java
+++ Covid_vaccination_service/src/main/java/com/coviwin/model/VaccineCount.java
@@ -1,4 +1,4 @@
-package com.masai.model;
+package com.coviwin.model;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -9,6 +9,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.masai.model.Vaccine;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
