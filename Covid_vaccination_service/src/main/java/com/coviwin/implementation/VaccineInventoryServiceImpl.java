3a5fb34a1132a42142379487a4acc18ee459e408
Aman Rawat <Amansinghrawat361@gmail.com>
2022-11-11 19:38:06 +0530

    all implementation done

--- Covid_vaccination_service/src/main/java/com/coviwin/implementation/VaccineInventoryServiceImpl.java
+++ Covid_vaccination_service/src/main/java/com/coviwin/implementation/VaccineInventoryServiceImpl.java
@@ -1,21 +1,29 @@
 package com.coviwin.implementation;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.coviwin.exception.VaccineInventoryException;
+import com.coviwin.model.VaccinationCenter;
 import com.coviwin.model.Vaccine;
 import com.coviwin.model.VaccineCount;
 import com.coviwin.model.VaccineInventory;
+import com.coviwin.repo.VaccinationCenterRepo;
 import com.coviwin.repo.VaccineInventoryRepo;
 import com.coviwin.service.VaccineInventoryService;
 
+@Service
 public class VaccineInventoryServiceImpl implements VaccineInventoryService {
 
 	@Autowired
 	private VaccineInventoryRepo vacInvRepo;
+	
+	@Autowired
+	private VaccinationCenterRepo vacCenterRepo;
 	
 	
 	@Override
@@ -29,6 +37,7 @@ public class VaccineInventoryServiceImpl implements VaccineInventoryService {
 			return list;
 	}
 
+	
 	@Override
 	public VaccineInventory addVaccineCount(VaccineInventory vacInv, VaccineCount vacineCount)throws VaccineInventoryException {
 
@@ -47,33 +56,114 @@ public class VaccineInventoryServiceImpl implements VaccineInventoryService {
 	@Override
 	public VaccineInventory addVaccineInventoryByCenter(Integer centerId, VaccineInventory vInventory)
 			throws VaccineInventoryException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		VaccinationCenter vaccinationCenter = vacCenterRepo.findById(centerId)
+				                              .orElseThrow(() -> new VaccineInventoryException("No VaccinationCenter found with centerId : "+ centerId)) ;
+	
+	    vaccinationCenter.setVaccineInventory(vInventory); // associating vaccinationCenter with VaccineInventory
+	    
+	    List<VaccineCount> VacCountList = vInventory.getVaccineCounts();
+	    
+	    for(VaccineCount vacCount : VacCountList) {
+	    	
+	    	vacCount.setVaccineInventory(vInventory); // associating each VaccineCount with VaccineInventory
+	    }
+	    
+	    
+	    vInventory.getVaccinationCenters().add(vaccinationCenter);
+	    
+	    return vacInvRepo.save(vInventory);
+	
 	}
 
 	@Override
 	public VaccineInventory updateVaccineInventory(Integer vaccineInventory, VaccineInventory vInventory)
 			throws VaccineInventoryException {
-		// TODO Auto-generated method stub
-		return null;
+
+          vacInvRepo.findById(vaccineInventory).orElseThrow(() -> new VaccineInventoryException("No VaccineInventory found with Id : "+vaccineInventory));
+          
+          
+          List<VaccinationCenter> centerList = vInventory.getVaccinationCenters();
+          
+          for(VaccinationCenter vacCenter : centerList) {
+        	  
+        	  vacCenter.setVaccineInventory(vInventory); // associating each VaccinationCenter with updated VaccineInventory
+          }
+          
+          
+          List<VaccineCount> VacCountList = vInventory.getVaccineCounts();
+  	    
+  	    for(VaccineCount vacCount : VacCountList) {
+  	    	
+  	    	vacCount.setVaccineInventory(vInventory); // associating each VaccineCount with updated VaccineInventory
+  	    }
+  	    
+          
+          return vacInvRepo.save(vInventory);
+          
 	}
 
 	@Override
 	public Boolean deleteVaccineInventory(Integer vaccineInventory) throws VaccineInventoryException {
-		// TODO Auto-generated method stub
-		return null;
+
+        vacInvRepo.findById(vaccineInventory).orElseThrow(() -> new VaccineInventoryException("No VaccineInventory found with Id : "+vaccineInventory));
+
+        vacInvRepo.deleteById(vaccineInventory);
+        
+        return true;
 	}
 
 	@Override
 	public List<VaccineInventory> getVaccineInventoryByDate(LocalDate date) throws VaccineInventoryException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<VaccineInventory> list = vacInvRepo.findBydate(date);
+		
+		if(list.isEmpty()) {
+			throw new VaccineInventoryException("No VaccineInventory found with data : " + date);
+		}else
+			return list;
+		
 	}
 
 	@Override
 	public List<VaccineInventory> getVaccineInventoryByVaccine(Vaccine vaccine) throws VaccineInventoryException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<VaccineInventory> result = new ArrayList<>();
+		
+		List<VaccineInventory> list = vacInvRepo.findAll();
+		
+		if(list.isEmpty()) {
+			throw new VaccineInventoryException("No VaccineInventory found, first add some Inventory");
+			
+		}
+			
+			for(VaccineInventory vacInv : list) {
+				
+			  List<VaccineCount> vacCountList =	vacInv.getVaccineCounts();
+			  
+			  if(vacCountList.isEmpty()) {
+				   throw new VaccineInventoryException("No VaccineCount found, first add some VaccineCount");
+			  
+			  }
+				  
+				  for(VaccineCount vacCount : vacCountList) {
+					  
+					  if(vacCount.getVaccine() == vaccine) {
+						  result.add(vacInv);
+					  }
+					  
+				  }
+				
+			}
+			
+		
+		
+		if(result.isEmpty()) {
+			   throw new VaccineInventoryException("No VaccineInventory found with Vaccine : " + vaccine);
+
+		}else
+			return result;
+		
 	}
 
 }
